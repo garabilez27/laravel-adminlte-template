432610c01d1dcdd6b4911388c184bd64225e4f39
Wendhelle Hafalla Garabilez <garabilez.wendhelle@spcc.edu.ph>
2024-12-02 08:57:30 +0800

    Added Middlewares

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function index()
+    {
+        return view('pages.login');
+    }
+
+    public function add()
+    {
+        return view('pages.register');
+    }
+
+    public function reset()
+    {
+        return view('pages.forgot-password');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('pages.forgot-password');
+use App\Http\Middleware\Authorized;
+use App\Http\Middleware\Guest;
+
+use App\Http\Controllers\LoginController;
+
+Route::middleware(Authorized::class)->group(function() {
+
+});
+
+Route::middleware(Guest::class)->group(function() {
+    Route::get('/', [LoginController::class, 'index'])->name('signin');
+    Route::get('/add', [LoginController::class, 'add'])->name('signup');
+    Route::get('/reset', [LoginController::class, 'reset'])->name('reset');
 });
